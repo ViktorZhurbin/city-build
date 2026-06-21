cd4f5adbd0392823b7e5317207ec795108358a6d
Viktor Zhurbin <34838167+ViktorZhurbin@users.noreply.github.com>
2026-06-21 16:06:42 +0200

    set up rsdoctor

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginBabel } from "@rsbuild/plugin-babel";
 import { pluginSolid } from "@rsbuild/plugin-solid";
+import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
@@ -10,4 +11,14 @@ export default defineConfig({
 		}),
 		pluginSolid(),
 	],
+	tools: {
+		rspack: {
+			plugins: [
+				process.env.RSDOCTOR === "true" &&
+					new RsdoctorRspackPlugin({
+						// plugin options
+					}),
+			],
+		},
+	},
 });
